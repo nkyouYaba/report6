3dfd9ff8b1f322eb04c5e098a4262e11979d3774
Kyosuke Nakama <e235752@ie.u-ryukyu.ac.jp>
2024-01-16 15:04:33 +0900

    2nd commit

--- app/src/main/java/Battle.java
+++ app/src/main/java/Battle.java
@@ -1,3 +1,5 @@
+import jp.ac.uryukyu.ie.e235752.*;
+
 public class Battle {
     public static void main(String[] args) {
         
--- app/src/main/java/jp/ac/uryukyu/ie/e235752/Skill.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e235752/Skill.java
@@ -1,5 +1,24 @@
 package jp.ac.uryukyu.ie.e235752;
 
 public class Skill {
-    
+    String name;
+    String skillPattern;
+    int attackAmount;
+    int attackType;
+
+    Skill(String name, String skillPattern, int attackType, int attackAmount) {
+        this.name = name;
+        this.skillPattern = skillPattern;
+        this.attackType = attackType;
+        this.attackAmount = attackAmount;
+    }
+
+    public void attack(Pokemon partnerPokemon, Pokemon opponentPokemon){
+        if(skillPattern == "物理"){
+            opponentPokemon.hitPoint -= Math.floor(22 * Math.floor(attackAmount * partnerPokemon.attackOfPhisical / opponentPokemon.diffenceOfPhisical) / 50 + 2);
+        }
+        if(skillPattern == "特殊"){
+            opponentPokemon.hitPoint -= Math.floor(22 * Math.floor(attackAmount * partnerPokemon.attackOfSpecial / opponentPokemon.diffenceOfSpecial) / 50 + 2);
+        }
+    }
 }
